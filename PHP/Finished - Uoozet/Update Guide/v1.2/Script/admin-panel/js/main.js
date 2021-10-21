b173fef9a18f65f1acf134dfd5c984fd8647792e
Alireza Sharifikia <alireza.sharifikia@gmail.com>
2021-10-21 14:08:08 +0900

    chore: C++ Branch Initialized

--- PHP/Finished - Uoozet/Update Guide/v1.2/Script/admin-panel/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.delete-button').on('click', function(event) {
-        event.preventDefault();
-        var ajax_type = $(this).attr('data-type');
-        var id = $('#delete-modal').attr('data-id');
-        $('#list-' + id).remove();
-        $.post('{{LINK ajax/admin/delete-' + ajax_type + '}}', {id: id});
-    });
-});
