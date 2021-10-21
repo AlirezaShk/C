03f9823227477fe7ed60000acd20ce305bbca2c2
Alireza Sharifikia <alireza.sharifikia@gmail.com>
2021-10-21 14:18:47 +0900

    chore: Python branch initialized

--- PHP/Finished - Uoozet/sources/audio_book/content.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pt->page_url_ = $pt->config->site_url.'/audio_book';
-$pt->page = 'کتاب صوتی';
-$pt->title = 'کتاب صوتی | ' . $pt->config->title;
-$pt->description = $pt->config->description;
-$pt->keyword = $pt->config->keyword;
-$pt->content = PT_LoadPage('audio_book/content');
